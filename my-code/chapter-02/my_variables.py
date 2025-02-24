330ae4c741b23cded81054b4116611a03b5fee3a
MiekeTechGeek <miekepienaar435@gmail.com>
2025-02-24 21:27:40 +0200

    first commit

---
+++ my-code/chapter-02/my_variables.py
@@ -0,0 +1,18 @@
+name = "Mieke"
+
+surname = "Pienaar"
+
+my_sentence = f"Hello my name is {name} {surname}."
+
+print(f"\t{my_sentence}")
+
+print("I like music. \nI like horse riding. \nI like chinchillas.")
+
+print("              Mieke".lstrip())
+
+my_link = "https://codecollege.co.za"
+print(my_link.removeprefix("https://"))
+
+# Multiple Assignments
+x, y, z, w = 1, 2, 3, 4
+print(x, y, z, w)
